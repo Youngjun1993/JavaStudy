ba6b328818bbc1f37d05dde2cdc1feaf9f022909
Lee Youngjun <zerojunee@naver.com>
2021-01-10 19:20:29 +0900

    2021-01-10 수정

--- BookManagement/src/adminSystemData/AdminDataSet.java
+++ BookManagement/src/adminSystemData/AdminDataSet.java
@@ -6,11 +6,11 @@ public class AdminDataSet {
 	public static HashMap<String, AdminVO> userList = new HashMap<String, AdminVO>();
 	public AdminDataSet() {}
 	public static void setUserList() {
-		userList.put("1", new AdminVO("1","유저1","010-1111-1111","가동",1));
-		userList.put("4", new AdminVO("4","유저4","010-4444-4444","라동",4));
-		userList.put("2", new AdminVO("2","유저2","010-2222-2222","나동",2));
-		userList.put("3", new AdminVO("3","유저3","010-3333-3333","다동",3));
-		userList.put("5", new AdminVO("5","유저1","010-5555-5555","마동",5));
+		userList.put("1", new AdminVO("1","장성훈","010-1548-9058","가동",6));
+		userList.put("4", new AdminVO("4","오은비","010-3568-4784","나동",8));
+		userList.put("2", new AdminVO("2","이태성","010-1645-3548","다동",6));
+		userList.put("3", new AdminVO("3","박단비","010-2268-2245","라동",12));
+		userList.put("5", new AdminVO("5","김영진","010-7878-9295","마동",22));
 	}
 	
 }
